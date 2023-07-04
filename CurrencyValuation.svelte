7e4d2ecffbe53caed09d40e2471196447467b1da
we_are_but_neutrinos <adithyasudhan@gmail.com>
2023-07-04 14:01:36 +0000

    add UI update to display more info

--- CurrencyValuation.svelte
+++ CurrencyValuation.svelte
@@ -1,38 +1,25 @@
 <script>
+  import { onMount } from "svelte";
+
   export let countries;
   export let selectedCountry1;
   export let selectedCountry2;
-  let impliedExchangeRate;
   let exchangeRate;
-  let bigMacPrices;
-  let foreignPrice;
-  let price1;
-  let price2;
+  let valuation;
+  const bigMacPrices = { "CAD": 6.77, "CNY": 24, "INR": 191, "USD": 5.15, "EUR": 4.65, "CHF": 6.5 }; // hardcoded
 
-  async function fetchExchangeRate() {
-    const response = await fetch(
-      `https://api.exchangerate-api.com/v4/latest/${selectedCountry1.code}`
-    );
+  async function fetchExchangeRate(foreignCountry) {
+    const response = await fetch(`https://api.exchangerate-api.com/v4/latest/${selectedCountry1.code}`);
     const data = await response.json();
-    exchangeRate = data.rates[selectedCountry2.code];
-    
-    // const bigMacResponse = await fetch('https://api.example.com/bigmacindex');
-    // const bigMacData = await bigMacResponse.json();
-    bigMacPrices = {"CAD": 6.77, "CNY": 24, "INR": 191, "USD": 5.15, "EUR": 4.65 } //hardcoded
+    return data.rates[foreignCountry];
   }
 
-function calculateValuation() {
-  if (!exchangeRate) return "";
-  console.log("selectedCountry2.code: " + selectedCountry2.code);
+  function calculateValuation() {
+    const price1 = bigMacPrices[selectedCountry1.code];
+    const price2 = bigMacPrices[selectedCountry2.code];
 
-  price1 = bigMacPrices[selectedCountry1.code];
-  price2 = bigMacPrices[selectedCountry2.code];
-
-  if (price1 && price2) {
-    impliedExchangeRate = price2 / price1;
-    console.log("implied: " + impliedExchangeRate);
-    console.log("real: " + exchangeRate)
-    foreignPrice =  price2/exchangeRate;
+    const impliedExchangeRate = price2 / price1;
+    const foreignPrice = price2 / exchangeRate;
 
     if (impliedExchangeRate > exchangeRate) {
       return `${selectedCountry2.code} is overvalued relative to ${selectedCountry1.code}`;
@@ -41,47 +28,48 @@ function calculateValuation() {
     } else {
       return `${selectedCountry2.code} is fairly valued relative to ${selectedCountry1.code}`;
     }
-  } else {
-    return "";
   }
-}
 
- $: {
-    if (selectedCountry1 || selectedCountry2) {
-      fetchExchangeRate().then(rate => {
-        exchangeRate = rate;
-        calculateValuation();
-      });
-    }
- }
-  
+  async function handleCountryChange() {
+    exchangeRate = await fetchExchangeRate(selectedCountry2.code);
+    valuation = calculateValuation();
+  }
+
+ 
+
+  onMount(async () => {
+    await handleCountryChange();
+    valuation = calculateValuation();
+  });
 </script>
 
 <main>
   <h1>Big Mac Index Currency Valuation</h1>
-  <select bind:value={selectedCountry1}>
+  <select bind:value={selectedCountry1} on:change={handleCountryChange}>
     {#each countries as country}
-      <option value={country}>{country.name}</option>
+    <option value={country}>{country.name}</option>
     {/each}
   </select>
-  <select bind:value={selectedCountry2}>
+  <select bind:value={selectedCountry2} on:change={handleCountryChange}>
     {#each countries as country}
-      <option value={country}>{country.name}</option>
+    <option value={country}>{country.name}</option>
     {/each}
   </select>
+
+  {#if valuation}
+  <p>
+    Valuation: {valuation}
+    <br>
+    Exchange rate: {exchangeRate}
+    <br>
+    Implied Exchange rate: {bigMacPrices[selectedCountry2.code] / bigMacPrices[selectedCountry1.code]}
+  </p>
 
   <p>
-    {#if exchangeRate}
-      Valuation: {calculateValuation()}
-      
-      Exchange rate: {exchangeRate}
-      Implied Exchange rate: {impliedExchangeRate}
-    {/if}
-    {#if bigMacPrices}
-      A Big Mac bought in {selectedCountry1.name} for {price1} {selectedCountry1.code} would be valued at {foreignPrice} {selectedCountry1.code} in {selectedCountry2.name}!
-    {/if}
-
+    A Big Mac bought in {selectedCountry1.name} for {bigMacPrices[selectedCountry1.code]} {selectedCountry1.code}
+    would be valued at {bigMacPrices[selectedCountry2.code] / exchangeRate} {selectedCountry1.code} in {selectedCountry2.name}
   </p>
+  {/if}
 </main>
 
 <style>
